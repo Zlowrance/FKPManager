028fce21b1434645682c3cfd6339e1ba5c48948b
ZachL <zachlowrance@gmail.com>
2023-11-24 07:54:10 -0600

    Broadcast latest data to guild addon channel when updated, and apply changes to local data if guildie data is fresher than yours

--- App.lua
+++ App.lua
@@ -1,6 +1,10 @@
 -- Author      : zachl
 -- Create Date : 11/14/2023 11:42:58 AM
 
+local MSG_PREFIX = "FKPManager"
+local receivedChunks = {}
+local expectedTotal = nil
+
 -- SLASH COMMANDS
 
 SLASH_FKP1 = "/fkp"
@@ -18,21 +22,95 @@ local eventFrame = CreateFrame("Frame", nil, UIParent)
 eventFrame:Hide()  
 
 eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:RegisterEvent("CHAT_MSG_ADDON")
 
-eventFrame:SetScript("OnEvent", function(self, event, arg1)
-    if event == "ADDON_LOADED" and arg1 == ADDON_NAME then
-        if FKPManagerData == nil then
-		    FKPManagerData = {DataTimestamp = 0, FKPSpent = {}, PastBids = {}}
-		end
-        
-        if FKPManagerData.DataTimestamp < FKPDataLastUpdated then
-            FKPManagerData.DataTimestamp = FKPDataLastUpdated
-            FKPManagerData.FKPSpent = {}
-            Log("Data updated, cleared cache")
+local function SendChunksToGuild(chunks)
+    for _, chunk in ipairs(chunks) do
+        C_ChatInfo.SendAddonMessage(MSG_PREFIX, chunk, "PARTY", nil)
+    end
+end
+
+local function OnLoaded()
+    if FKPManagerData == nil then
+        FKPManagerData = {DataTimestamp = 0, FKPSpent = {}, PastBids = {}}
+    end
+
+    if FKPManagerData.DataTimestamp < FKPDataLastUpdated then
+        FKPManagerData.DataTimestamp = FKPDataLastUpdated
+        FKPManagerData.FKPSpent = {}
+        Log("Data updated, cleared cache. Sending updated data to guild.")
+        local chunks = FKPHelper:SerializeAndChunk()
+        SendChunksToGuild(chunks)
+    end
+
+    C_ChatInfo.RegisterAddonMessagePrefix(MSG_PREFIX)
+
+    Log("FKPManager loaded: ")
+    Log(DumpTable(FKPManagerData))
+end
+
+local function OnAddonMessage(prefix, message, channel, sender)
+    if prefix ~= MSG_PREFIX then
+        return
+    end
+    
+    if DEBUG then
+        Log("Received addon message: " .. message)
+    end
+    
+    local part, total, chunk = string.match(message, "(%d+)/(%d+):(.*)")
+    part, total = tonumber(part), tonumber(total)
+
+    -- Initialize when first chunk is received
+    if part == 1 then
+        receivedChunks = {}
+        expectedTotal = total
+    end
+
+    -- Ignore if total doesn't match expected
+    if total ~= expectedTotal then 
+        return 
+    end
+
+    -- Store the chunk
+    receivedChunks[part] = chunk
+
+    -- Check if all chunks are received
+    if #receivedChunks == total then
+        local fullMessage = table.concat(receivedChunks)
+        local deserialized = FKPHelper:Deserialize(fullMessage)
+        if not deserialized then
+            Log("Failed to deserialize message")
+            return
         end
-     
-        self:UnregisterEvent("ADDON_LOADED")  
-        Log("FKPManager loaded: ")
-        Log(DumpTable(FKPManagerData))
+        if deserialized.DataTimestamp > FKPManagerData.DataTimestamp then
+            Log("Received new data")
+            -- for each player in the new data, apply an FKP delta to local storage for the difference between the new and old FKP
+            FKPManagerData.FKPSpent = {}
+            FKPManagerData.DataTimestamp = deserialized.DataTimestamp
+            for playerName, fkp in pairs(deserialized.FKPData) do
+                Log("Player " .. playerName .. " has " .. fkp .. " FKP")
+                local oldFKP = FKPHelper:GetFKP(playerName)
+                if oldFKP ~= nil then
+                    local delta = fkp - oldFKP
+                    FKPManagerData.FKPSpent[playerName] = -delta
+                end
+            end
+        else
+            Log("Received data is older than current data. New timestamp: " .. deserialized.DataTimestamp .. ", current timestamp: " .. FKPManagerData.DataTimestamp)
+        end
+    end
+end
+
+eventFrame:SetScript("OnEvent", function(self, event, ...)
+    if event == "ADDON_LOADED" then
+        local addonName = ...
+        if addonName == ADDON_NAME then
+            OnLoaded()
+            self:UnregisterEvent("ADDON_LOADED")
+        end
+    elseif event == "CHAT_MSG_ADDON" then
+        local prefix, message, channel, sender = ...
+        OnAddonMessage(prefix, message, channel, sender)
     end
 end)
--- FKPHelper.lua
+++ FKPHelper.lua
@@ -1,6 +1,8 @@
 -- Author      : zachl
 -- Create Date : 11/14/2023 2:58:00 PM
 
+local DATA_UPDATED_KEY = "DATALASTUPDATED"
+
 FKPHelper = {}
 
 function FKPHelper:GetFKP(playerName)
@@ -31,4 +33,41 @@ end
 
 function FKPHelper:GetPastBids()
     return FKPManagerData.PastBids or {}
+end
+
+
+function FKPHelper:SerializeAndChunk()
+    local serialized = DATA_UPDATED_KEY .. ":" .. FKPDataLastUpdated .. ";"
+    for key, value in pairs(FKPData) do
+        serialized = serialized .. key .. ":" .. value .. ";"
+    end
+
+    -- 255 - 10 chunk size for header and safety
+    local chunkSize = 245
+    local chunks = {}
+    local part = 1
+    local total = math.ceil(#serialized / chunkSize)
+
+    for i = 1, #serialized, chunkSize do
+        local chunk = part .. "/" .. total .. ":" .. string.sub(serialized, i, i + chunkSize - 1)
+        table.insert(chunks, chunk)
+        part = part + 1
+    end
+
+    return chunks
+end
+
+function FKPHelper:Deserialize(serializedString)
+    local newData = {}
+    local timestamp = 0
+
+    for key, value in string.gmatch(serializedString, "(%w+):(%d+);") do
+        if key == DATA_UPDATED_KEY then
+            timestamp = tonumber(value)
+        else
+            newData[key] = tonumber(value)
+        end
+    end
+
+    return {FKPData = newData, DataTimestamp = timestamp}
 end
